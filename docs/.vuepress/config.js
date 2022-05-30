268db705e19125d23ac5c96d8cac0f6780c4f8a0
Atlinna <53805378+atlinna@users.noreply.github.com>
2022-05-30 15:14:16 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,10 +5,35 @@ module.exports = {
     description: '互相探讨，共同学习',
     repo: '',
     base: '',
+    theme: 'reco',
     lastUpdated: 'Last Updated',
     themeConfig: {
         sidebar: utils.getContents(),
         logo: '',
         nav: utils.getTableBars()
-    }
+    },
+    plugins: [
+        [
+            '@vuepress-reco/vuepress-plugin-kan-ban-niang', {
+                theme: ['blackCat'],
+                clean: false,
+                messages: {
+                    welcome: '欢迎来到我的博客',
+                    home: '这是我家嗷',
+                    theme: '你在狗叫什马东西？',
+                    close: '玛德，老子不伺候了'
+                },
+                messageStyle: { right: '68px', bottom: '290px' },
+                width: 250,
+                height: 320
+            }
+        ],
+        [
+            'vuepress-plugin-cursor-effects', {
+                size: 2,
+                shape: 'star',
+                zIndex: 9999999999
+            }
+        ]
+    ]
 }
