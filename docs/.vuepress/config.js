6b237df197e5ebfa9e0e316825c7b2e191f81f5e
Atlinna <53805378+atlinna@users.noreply.github.com>
2022-06-08 16:04:42 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 const utils = require('./utils/index.js')
 module.exports = {
-    title: 'start to life',
+    title: '每一个不曾起舞的日子，都是对生命的辜负',
     keywords: '前端开发',
     description: '互相探讨，共同学习',
     repo: '',
@@ -13,21 +13,6 @@ module.exports = {
         nav: utils.getTableBars()
     },
     plugins: [
-        [
-            '@vuepress-reco/vuepress-plugin-kan-ban-niang', {
-                theme: ['blackCat'],
-                clean: false,
-                messages: {
-                    welcome: '欢迎来到我的博客',
-                    home: '这是我家嗷',
-                    theme: '你在狗叫什马东西？',
-                    close: '玛德，老子不伺候了'
-                },
-                messageStyle: { right: '68px', bottom: '290px' },
-                width: 250,
-                height: 320
-            }
-        ],
         [
             'vuepress-plugin-cursor-effects', {
                 size: 2,
