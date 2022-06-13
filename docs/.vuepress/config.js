ee2711e6cacbe72ea4e121e27c6d51b3eadbef4a
Atlinna <53805378+atlinna@users.noreply.github.com>
2022-06-13 17:33:45 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,17 @@
 const utils = require('./utils/index.js')
 module.exports = {
-    title: '每一个不曾起舞的日子，都是对生命的辜负',
+    title: 'Mr.Zhou（周始）',
     keywords: '前端开发',
-    description: '互相探讨，共同学习',
+    description: '梦开始的地方',
     repo: '',
     base: '',
-    author:'周始',
-    record:'not yet haha···',
-    startYear:'2022',
+    author: '周始',
+    record: 'not yet haha···',
+    startYear: '2022',
     theme: 'reco',
     lastUpdated: 'Last Updated',
     themeConfig: {
-        huawei:true,
+        huawei: true,
         sidebar: utils.getContents(),
         logo: '',
         nav: utils.getTableBars()
@@ -22,6 +22,23 @@ module.exports = {
                 size: 2,
                 shape: 'star',
                 zIndex: 9999999999
+            }
+        ],
+        ["vuepress-plugin-boxx"],
+        ["vuepress-plugin-nuggets-style-copy", {
+            copyText: "复制代码",
+            tip: {
+                content: "复制成功!"
+            }
+        }],
+        [
+            "dynamic-title",
+            {
+                showIcon: "/favicon.ico",
+                showText: "(/≧▽≦/)咦！又好了！",
+                hideIcon: "/failure.ico",
+                hideText: "(●—●)喔哟，崩溃啦！",
+                recoverTime: 2000
             }
         ]
     ]
