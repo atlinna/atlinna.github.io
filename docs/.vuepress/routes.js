f1a5bdb94c6fe433022d1f45b8d0500e4eadb628
Lucas Y <1303117237@qq.com>
2023-04-19 00:03:59 +0800

    【MODIFY】修改文档内容，扩充领域内容

--- docs/.vuepress/routes.js
+++ docs/.vuepress/routes.js
@@ -1,12 +1,21 @@
+// module.exports = [
+//     // 导航栏
+//     { text: '首页', link: '/' },
+//     { text: 'HTML', link: '/html_docs/' },
+//     { text: 'CSS', link: '/css_docs/' },
+//     { text: 'JS', link: '/js_docs/' },
+//     { text: 'Vue', link: '/Vue/' },
+//     { text: '设计模式', link: '/设计模式/' },
+//     { text: '数据结构与算法', link: '/problem/' },
+//     { text: '手写系列', link: '/手写/' },
+//     { text: '其他', link: '/Other/' },
+// ]
+
 module.exports = [
-    // 导航栏
-    { text: '首页', link: '/' },
-    { text: 'HTML', link: '/html_docs/' },
-    { text: 'CSS', link: '/css_docs/' },
-    { text: 'JS', link: '/js_docs/' },
-    { text: 'Vue', link: '/Vue/' },
-    { text: '设计模式', link: '/设计模式/' },
-    { text: '数据结构与算法', link: '/problem/' },
-    { text: '手写系列', link: '/手写/' },
-    { text: '其他', link: '/Other/' },
-]
+  { text: "首页", link: "/" },
+  { text: "前端领域", link: "/前端领域/" },
+  { text: "Python", link: "/Python/" },
+  { text: "设计模式", link: "/设计模式/" },
+  { text: "移动端", link: "/移动端/" },
+  { text: "服务器类", link: "/服务器类/" },
+];
--- docs/Other/index.md
+++
@@ -1,3 +0,0 @@
-一句 hello world 送给各位
-![abcd](https://github.com/atlinna/viteAndWebpack/raw/main/src/assets/logo.png)
-![efgh](https://github.com/atlinna/viteAndWebpack/blob/main/src/assets/logo.png?raw=true)
